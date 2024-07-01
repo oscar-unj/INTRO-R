1151ed625b7401c4e054e82c03db749283f5f635
oscar-unj <135717025+oscar-unj@users.noreply.github.com>
2024-07-01 14:38:16 -0500

    Add files via upload

---
+++ Ejm-04/renderizar_markdown.r
@@ -0,0 +1,12 @@
+
+library(rmarkdown)
+
+# Renderizar el documento en PDF
+render("doc_markdown.rmd", output_format = "pdf_document")
+
+###  REFERENCIA
+###  OpenAI (2024). ChatGPT. (Versión del 01 de Julio)
+###  [Render PDF using RMarkdown ].
+###  https://chatgpt.com/share/0c5530b8-f01b-4f6c-a4a7-3897ad8d0f4a
+
+
