df19d991603bc9c0ea8a0e9b4b1e8e44c9c3956e
oscar-unj <135717025+oscar-unj@users.noreply.github.com>
2024-07-03 12:36:20 -0500

    Add files via upload

---
+++ Ejm-04/instalar_markdown.r
@@ -0,0 +1,12 @@
+# Solo se instala una vez.
+install.packages("rmarkdown")
+install.packages("knir")
+install.packages("pandoc")
+install.packages("tinytex")
+
+###  Basado en:
+###  OpenAI (2024). ChatGPT. (Versión del 01 de Julio)
+###  [Render PDF using RMarkdown ].
+###  https://chatgpt.com/share/0c5530b8-f01b-4f6c-a4a7-3897ad8d0f4a
+
+
