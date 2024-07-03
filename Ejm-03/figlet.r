297c578bfcf1991a1fa173707e023d761f1a1491
oscar-unj <135717025+oscar-unj@users.noreply.github.com>
2024-07-03 12:26:31 -0500

    Add files via upload

---
+++ Ejm-03/figlet.r
@@ -0,0 +1,18 @@
+# Rich FitzJohn (s.f.)
+# rfiglet https://richfitz.github.io/rfiglet/
+#----------------------------------
+# archivo:  figlet.r
+#
+# Entrar en R e instalar: 
+# remotes::install_github("richfitz/rfiglet", upgrade = FALSE)
+
+library(rfiglet)
+
+figlet("Intro   a   R")
+
+
+
+
+
+
+
