5cadfdccaf4ecd02df5f4c044017afc2cb060cf3
oscar-unj <135717025+oscar-unj@users.noreply.github.com>
2024-07-04 22:06:01 -0500

    Delete Ejm-12/instalar.r

--- Ejm-12/instalar.r
+++
@@ -1,9 +0,0 @@
-# Instalación y carga de librerías necesarias
-# para actualizar el repositorio de cran
-options(repos = c(CRAN = "https://cran.r-project.org")) 
-install.packages("dplyr")
-install.packages("ggplot2")
-install.packages("survival")
-install.packages("MASS")
-
-
