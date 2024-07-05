724ea617abd15f5305f5486a5f1c916d902bb1a8
oscar-unj <135717025+oscar-unj@users.noreply.github.com>
2024-07-04 22:06:46 -0500

    Add files via upload

---
+++ Ejm-12/instalar.r
@@ -0,0 +1,9 @@
+# Instalación de librerías necesarias
+# para actualizar el repositorio de cran
+options(repos = c(CRAN = "https://cran.r-project.org")) 
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("survival")
+install.packages("MASS")
+
+
